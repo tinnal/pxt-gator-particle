10174ade18f72a85087911d077e2ebd35bb692a5
Andrew England <andrew.england@sparkfun.local>
2019-05-07 14:52:06 -0600

    probing temp2[0]

--- MAX30105.cpp
+++ MAX30105.cpp
@@ -658,7 +658,7 @@ uint16_t MAX30105::check(void)
 			switch (led)
 			{
 				case 0:
-					sense.red[sense.head] = tempLong;//Store this reading into the sense array
+					sense.red[sense.head] = temp2[0];//Long;//Store this reading into the sense array
 					break;
 				case 1:
 					sense.IR[sense.head] = tempLong;
